1bcea51aed6523a1ef238fe2a7c80999276bacf5
Mathieu Vignau <mathieu.vignau@ff0000.com>
2018-05-08 00:43:22 -0700

    Added WS digest

--- ios/SBDebug/Credentials.swift
+++ ios/SBDebug/Credentials.swift
@@ -9,7 +9,7 @@
 class Credentials {
     static let RPC_USER = "app"
     static let RPC_PASSWORD = "likethat"
-    static let RPC_DOMAIN = "game7labs_sb"
-    static let RPC_HOST = "192.168.7.1"
-    static let RPC_URI = "/rpc/"
+    static let RPC_HOST = "192.168.1.7"
+    static let RPC_NETWORK = "192.168.1."
+    static let RPC_SSID = "Game 7 Scoreboard [1809A9]"
 }
--- ios/SBDebug/ViewController.swift
+++ ios/SBDebug/ViewController.swift
@@ -11,27 +11,33 @@ import SystemConfiguration
 import NetworkExtension
 import Reachability
 import SwiftyJSON
-import Starscream
 import NetUtils
 import SwiftHash
+import Starscream
 
 class ViewController: UIViewController, WebSocketDelegate {
+    func websocketDidConnect(socket: WebSocketClient) {
+        print("websocketDidConnect")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        print("websocketDidDisconnect")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        print("websocketDidReceiveData")
+    }
+    
 
     @IBOutlet weak var wifiIndicator: UIActivityIndicatorView!
     
-    var credentials = URLCredential(user: Credentials.RPC_USER, password: Credentials.RPC_PASSWORD, persistence: .forSession)
-    let protectionSpace = URLProtectionSpace(host: Credentials.RPC_HOST, port: 80, protocol: "http", realm: Credentials.RPC_DOMAIN, authenticationMethod: nil)
-    let session = URLSession(configuration: .default)
-    
-    let socket = WebSocket(url: URL(string: "ws://192.168.7.1/rpc")!)
-    
+    let socket = WebSocket(url: URL(string: String(format: "ws://%@/rpc", Credentials.RPC_HOST))!)
+    var socketAuth = JSON()
     let reachability = Reachability(hostname: Credentials.RPC_HOST)!
+    
     override func viewDidLoad() {
-        URLCredentialStorage.shared.setDefaultCredential(credentials, for: protectionSpace)
         
-        let wifi = NEHotspotConfiguration(ssid: "Game 7 Scoreboard [1809A9]")
-        wifi.joinOnce = true
-        NEHotspotConfigurationManager.shared.apply(wifi)
+//        NEHotspotConfigurationManager.shared.apply(NEHotspotConfiguration(ssid: Credentials.RPC_SSID))
         
         wifiIndicator.startAnimating()
         wifiIndicator.hidesWhenStopped = true
@@ -41,89 +47,85 @@ class ViewController: UIViewController, WebSocketDelegate {
         
         reachability.whenReachable = { reachability in
             for itf in Interface.allInterfaces() {
-                if itf.address?.range(of: "192.168.7.")?.lowerBound != nil {
+                if itf.address?.range(of: Credentials.RPC_NETWORK)?.lowerBound != nil {
                     self.wifiIndicator.stopAnimating()
-                    print("Reachable")
                     self.socket.connect()
                     return
                 }
             }
             self.wifiIndicator.startAnimating()
-            print("Not reachable")
         }
         reachability.whenUnreachable = { reachability in
             self.wifiIndicator.startAnimating()
-            print("Not reachable")
         }
         
         try? reachability.startNotifier()
     }
     
+    var callCount:UInt = 0
+    var callQueue = [RPCCall]()
+    
     func rpcCall(method:String, data:JSON?=nil, response:((JSON?)->Void)?=nil) {
-        /*
-        var request = URLRequest(url: URL(string: "http://"+Credentials.RPC_HOST+Credentials.RPC_URI+method)!)
-        request.httpMethod = "POST"
-        try? request.httpBody = data?.rawData()
-        session.dataTask(with: request) { (data, res, err) in
-            if let error = err {
-                print(error)
-                DispatchQueue.main.async {
-                    response?(nil)
-                }
-            } else if let data = data {
-                let result = JSON(data)
-                DispatchQueue.main.async {
-                    response?(result)
-                }
-            }
-        }.resume()
- */
-        var json:JSON = ["method":method, "src":"app", "auth":auth]
+        
+        callCount += 1
+        
+        let id = callCount
+        let call = RPCCall(callback: response, id: id, method: method, args: data)
+        callQueue.append(call)
+ 
+        var json:JSON = ["id":id, "method":method, "src":"app", "auth":socketAuth]
         if let data = data {
             json["args"] = data
         }
+        
         try? socket.write(data: json.rawData())
-    }
-    
-    func websocketDidConnect(socket: WebSocketClient) {
-        print("websocketDidConnect")
-    }
-    
-    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
-        print("websocketDidDisconnect", error)
-    }
-    
-    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
-        print("websocketDidReceiveData")
     }
     
     func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
         let result = JSON(parseJSON: text)
-        if let err = result["error"]["message"].string {
-            let errmsg = JSON(parseJSON: err)
-            print(errmsg)
-            if errmsg["auth_type"].string == "digest", let nonce = errmsg["nonce"].string, let nc = errmsg["nc"].string {
-                let ha1 = MD5(Credentials.RPC_USER+":"+Credentials.RPC_DOMAIN+":"+Credentials.RPC_PASSWORD)
-                let ha2 = MD5("GET:/rpc")
-                self.auth = MD5(ha1+":"+nonce+":"+nc+":"+ha2)
-                
-            
-                
+        let id = result["id"].uIntValue
+        if let index = callQueue.index(where: {$0.id == id}) {
+            let call = callQueue.remove(at: index)
+            if let err = result["error"]["message"].string {
+                let errmsg = JSON(parseJSON: err)
+                if errmsg["auth_type"].string == "digest", let nonce = errmsg["nonce"].number,
+                    let nc = errmsg["nc"].number, let realm = errmsg["realm"].string {
+                    socketAuth = generateDigestResponse(nonce: nonce, nc: nc, realm: realm)
+                    rpcCall(method: call.method, data: call.args, response: call.callback)
+                } else {
+                    print(err)
+                }
+            } else if let callback = call.callback {
+                callback(result["result"])
             }
         }
-        print(result)
+        
     }
     
-    var auth:String = ""
+    func generateDigestResponse(nonce:NSNumber, nc:NSNumber, realm:String) -> JSON {
+        let ha1 = MD5(String(format: "%@:%@:%@", Credentials.RPC_USER, realm, Credentials.RPC_PASSWORD)).lowercased()
+        let ha2 = MD5("dummy_method:dummy_uri").lowercased()
+        let cnonce = round(NSDate().timeIntervalSince1970)
+        let response = MD5(String(format: "%@:%@:%@:%.0f:auth:%@", ha1, nonce.stringValue, nc.stringValue, cnonce, ha2)).lowercased()
+        return ["realm":realm, "username":Credentials.RPC_USER, "nonce":nonce, "cnonce":cnonce, "response":response]
+    }
+ 
 
     @IBAction func onAction(_ sender: Any) {
-        self.rpcCall(method: "SB.SetScore", data: ["screen":UInt8(Double(arc4random())/Double(UInt32.max)*18)]) { response in
+        self.rpcCall(method: "SB.Next", data: ["screen":UInt8(Double(arc4random())/Double(UInt32.max)*18)]) { response in
             print(response)
         }
     }
     
     @IBAction func onDisconnect(_ sender: Any) {
         self.rpcCall(method: "Wifi.Disconnect")
+    }
+    
+    struct RPCCall {
+        let callback:((JSON?)->Void)?
+        let id:UInt
+        let method:String
+        let args:JSON?
     }
     
 }
